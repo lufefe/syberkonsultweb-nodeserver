552a38e9aa124a416245541df5a864495f323f3c
lufefe <jumbaayabonga@gmail.com>
2024-06-14 00:47:08 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,16 +4,13 @@ const axios = require('axios')
 const app = express()
 const port = 8000
 
-//todo store site secret key in aws
-const SITE_SECRET = process.env.SITE_SECRET;
-
 app.use(cors())
 app.use(express.json())
 
 app.post('/verify', async (request, response) => {
   const { captchaValue } = request.body
   const { data } = await axios.post(
-    `https://www.google.com/recaptcha/api/siteverify?secret=${SITE_SECRET}&response=${captchaValue}`,
+    `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.SITE_SECRET}&response=${captchaValue}`,
   )
   response.send(data)
 })
